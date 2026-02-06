58aaff1384a9aec88a332c21c2af5a6a992960b5
obg <a6ictive@gmail.com>
2026-02-06 12:22:53 +0000

    adding comments

--- stats.py
+++ stats.py
@@ -1,22 +1,19 @@
 import json, sys
 
-
-
 def sort_on(items):
     return items["num"]
 
 def get_num_words():
 
    with open(sys.argv[1]) as f: 
-      file_contents = f.read()
-      temp = file_contents.lower()
+      file_contents = f.read() # Reads the content of the file and saves it to a new variable
+      temp = file_contents.lower() # Creates a temp copy of the file_contents variable, with all chars set as lower case
 
-      temp.split()
+      temp.split() #Splitting the temp copy into a list
 
       mydict={}
 
-      
-      for length in temp:
+      for length in temp: # For loop iterating over each item in the temp list and counting each time an item appears
          try:
             mydict[length]+=1
          except:
@@ -33,9 +30,9 @@ def get_num_words():
       Analyzing book found at books/frankenstein.txt...""")
 
       print("""----------- Word Count ----------
-      Found""",len(temp.split()),"""total words""")
+      Found""",len(temp.split()),"""total words""") # splits temp into a list of words and gets the total length of the list
 
       print("""--------- Character Count -------""")
-      for x, y in sorted(mydict.items(), reverse=True, key=lambda item: item[1]):
+      for x, y in sorted(mydict.items(), reverse=True, key=lambda item: item[1]): #Loops through the key, value pairs in the dict, which is each character : how many times it appears
          if x.isalpha() == True:
             print(f"{x}: {y}")
